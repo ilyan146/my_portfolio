e291890f8c26bbbc16979bda01a172ce2af02f68
ilyan146 <ilyan146@gmail.com>
2023-03-27 01:22:25 +0400

    Deploying Commit 4 - adjusting for backslashes to become forward slach for getting images in streamlit cloud

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("006 images\my_photo4.jpeg")
+    st.image("006 images/my_photo4.jpeg")
 
 with col2:
     st.title("Mohamed Ilyan")
