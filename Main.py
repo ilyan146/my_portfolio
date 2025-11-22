f9c63596dd8d65fa42ce226684f92bef5a905836
Nazer, Mohamed Ilyan <mohamed.ilyan+Boskalis@boskalis.com>
2025-11-22 16:19:30 +0400

    Add more portfolio projects and improve the streamlit app UI

--- Main.py
+++ Main.py
@@ -4,43 +4,45 @@ import pandas as pd
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([1,3.5])
 
 with col1:
-    st.image("006 images/my_photo7.jpeg")
+    st.image("images/ilyan_image.jpeg", width=300)
+
 
 with col2:
     st.title("Mohamed Ilyan")
     content = """
-    Hi, I am Ilyan! I am a Chemical Engineer, Python programmer and Data Scientist. 
-    I graduated in 2021 with a Master of Science in Chemical
-    Engineering from the National University of Singapore with a focus
-    on Process Design, Programming for Data analysis and Simulation, and Project Engineering.
-    I have worked with organizations from various countries, such as Trust Your Water ME 
-    to provide water solutions for hotels, industrial projects and residentials; 
-    Biotech manufacturing at Geb Impact Technology Co. Ltd (Hong Kong); Provide 
-    reactor simulation using Python and MATLAB at the National University of Singapore.
+    Hi, I am Ilyan! An AI & Data Engineer.
+    I graduated in 2021 with a Master in Chemical Engineering from the National University of Singapore with a focus
+    on Programming for Data analysis and Simulation, Process Design, and Project Engineering.
+    My experience includes working with organizations from various countries and in different sectors,
+    including biotech/pharmaceutical manufacturing, water solutions, academic research and Civil Engineering.
+    My most recent role is as an AI & Data Engineer at Boskalis Westminister, a Marine Civil Engineering company 
+    where we leverage the data and AI/ML to create engineering software that helps in optimizing the operations, 
+    iterate to find solutions and for general automation and data management.
     """
     st.info(content)
 
-st.write("Below you find some of the apps I have built in Python, feel free to contact me.")
+st.write("---")
 
-df = pd.read_csv("006 data.csv")
+st.write("Below you'll find some of the apps I have built as a hobby, since I generally love to "
+         "develop new software in my free-time. If you'd love to know more, feel free to get in touch.")
 
-col3, empty_column, col4 = st.columns([1.5, 0.5, 1.5])
-#col3, empty_column, col4 = st.columns([1.5, 0.5, 1.5]), Original columns!
-#[4, 0.5, 1.5]
+df = pd.read_csv("app_data.csv")
 
-with col3:
+col4, empty_column, col5 = st.columns([1.5, 0.5, 1.5])
+
+with col4:
     for index, row in df[0:4].iterrows():
         st.header(row["title"])
         st.write(row["description"])
-        st.image("006 images/" + row["image"])
+        st.image("images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
 
-with col4:
+with col5:
     for index, row in df[4:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
-        st.image("006 images/" + row["image"])
+        st.image("images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -7,11 +7,11 @@ with st.form(key='email_forms'):
     email = st.text_input("Your Email Address")
     message = st.text_area("Your Message") #for multi line text
     raw_message = f"""\
-Subject: Email from portfolio
-
-From: {email}
-{message}
-"""
+    Subject: Email from portfolio
+    
+    From: {email}
+    {message}
+    """
     submit_button = st.form_submit_button("Submit") #special button that acts as boolean.
     if submit_button:
         send_email(raw_message)
